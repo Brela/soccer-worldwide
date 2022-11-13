67313314b83092b235a5b217b56bed227d820366
Breland <claybreland1@gmail.com>
2022-11-13 11:55:18 -0600

    working on table and API data

--- js/main.js
+++ js/main.js
@@ -5,35 +5,58 @@ document.querySelector('input').addEventListener('click', function handleClick(e
   event.preventDefault();
 });
 
-// CREATE TABLE
+var myHeaders = new Headers();
+myHeaders.append("x-rapidapi-key", "0cdf2e99564aaa2ac3de3c12f656fb29");
+myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
 
-function tableCreate() {
-  var body = document.getElementsByTagName('body')[0];
-  var tbl = document.createElement('table');
-  // tbl.style.width = '50%';
-  tbl.setAttribute('border', '1');
-  var tbdy = document.createElement('tbody');
-  for (var i = 0; i < 20; i++) {
-    var tr = document.createElement('tr');
-    // use this to create headings on first row - needs work
-    for (var j = 0; j < 10; j++) {
-      if (i == 0 && j == 0) {
-        tr.style.height = '50px'
-        break;
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
 
-      } else {
-        var td = document.createElement('td');
-        td.appendChild(document.createTextNode('\u0020'))
-        // i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
-        tr.appendChild(td)
-      }
-    }
-    tbdy.appendChild(tr);
-  }
-  tbl.appendChild(tbdy);
-  body.appendChild(tbl)
+// USER SELECTS LAGUE FROM DROPDOWN
+document.querySelector('.submit').addEventListener('click', function () {
+  var selectedLeague = ''
+  var e = document.getElementById('league');
+  selectedLeague = e.options[e.selectedIndex].text;
+  getFetchTopScorers(selectedLeague)
+})
+
+function getFetchTopScorers(selectedLeague) {
+  // learn switch and case
+  let pluginLeague = 39
+
+  fetch(`https://v3.football.api-sports.io/players/topscorers?season=2022&league=${pluginLeague}`, requestOptions)
+    .then(respons => respons.json())
+    .then(result => createTable(result.response))
+    .catch(error => console.log('error', error));
+
 }
-tableCreate();
+function createTable(result) {
+  result.forEach(el => {
+    console.log(el.player.photo)
+    let photo = el.player.photo
+    console.log(el.player) // object
+
+
+    console.log(el.player.name)
+    let pName = el.player.name
+    console.log(el.player.id)
+    let pId = el.player.id
+    console.log(el.player.nationality)
+    let pNation = el.player.nationality
+
+  })
+
+
+}
+
+// premier league ID = 39
+
+
+
+// TEMPLATE 
 
 /* function getFetch() {
   const choice = document.querySelector('input').value
@@ -49,24 +72,7 @@ tableCreate();
     });
 }  */
 
-var myHeaders = new Headers();
-myHeaders.append("x-rapidapi-key", "0cdf2e99564aaa2ac3de3c12f656fb29");
-myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
-// user selects league
-document.querySelector('.submit').addEventListener('click', function () {
-  var selectedLeague = ''
-  var e = document.getElementById('league');
-  selectedLeague = e.options[e.selectedIndex].text;
-  getFetchTopScorers(selectedLeague)
-})
-
+// GET LEAGUE ID
 
 // then carry over selected league and results
 /* function getLeagueID(selected, results) {
@@ -85,37 +91,36 @@ document.querySelector('.submit').addEventListener('click', function () {
   console.log(pLeague)
 } */
 
+// CREATE TABLE WITH TABLE DATA AS PPARAMETERS
+function tableCreate(photo, pName, pId, pNation) {
+  var body = document.getElementsByTagName('body')[0];
+  var tbl = document.createElement('table');
+  // tbl.style.width = '50%';
+  tbl.setAttribute('border', '1');
+  var tbdy = document.createElement('tbody');
 
-/* const results = function getFetch() {
+  for (var i = 0; i < 20; i++) {
+    var tr = document.createElement('tr');
 
-  fetch("https://v3.football.api-sports.io/leagues", requestOptions)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-    })
-    .catch(error => console.log('error', error));
+    // use this to create headings on first row - needs work
+    for (var j = 0; j < 10; j++) {
+      if (i == 0 && j == 0) {
+        tr.style.height = '50px'
+        break;
+
+        // this adds each column
+      } else {
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode('-'))
+        // i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+
+        //  this actually adds the column
+        tr.appendChild(td)
+      }
+    }
+    tbdy.appendChild(tr); // this actually adds the row
+  }
+  tbl.appendChild(tbdy);
+  body.appendChild(tbl)
 }
-
-console.log(results) */
-
-
-function getFetchTopScorers(selectedLeague) {
-  // learn switch and case
-  let pluginLeague = 39
-
-  fetch(`https://v3.football.api-sports.io/players/topscorers?season=2022&league=${pluginLeague}`, requestOptions)
-    .then(respons => respons.json())
-    .then(result => result.response.forEach(el => {
-      console.log(el.player.photo)
-      console.log(el.player) // object
-
-      console.log(el.player.name)
-      console.log(el.player.id)
-      console.log(el.player.nationality)
-    }))
-    .catch(error => console.log('error', error));
-}
-
-
-// premier league ID = 39
-
+tableCreate();
