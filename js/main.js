85ef3e7b7bb2b26889c5ad4cee11edb50391f380
Brela <claybreland1@gmail.com>
2023-10-27 18:55:59 -0500

    works

--- js/main.js
+++ js/main.js
@@ -15,10 +15,95 @@ const requestOptions = {
     const result = await response.json();
     return result.response;
   }
+
+  
+
+  // ---------------------------------------------------------------------------------------
+  function createTable(topScorers, leagueId) {
+    // Create table element and set its class and border attribute
+    const tbl = document.createElement("table");
+    tbl.className = "leagueId" + leagueId;
+    tbl.setAttribute("border", "1");
+
+    // Create table body element
+    const tbdy = document.createElement("tbody");
+
+    // Create table header row
+    const headerRow = document.createElement("tr");
+    headerRow.className = "headingRow";
+
+    // Create header cells containing the column headings and add them to the header row
+    const headerCells = ["", "", "", "Goals", "On Target", "Assists"];
+    headerCells.forEach((headerCell) => {
+      const td = document.createElement("td");
+      td.textContent = headerCell;
+      headerRow.appendChild(td);
+    });
+    tbdy.appendChild(headerRow);
+
+    // Function to add a row to the table (Moved out of the forEach loop)
+    function addRow(arr, pId) {
+      const tr = document.createElement("tr");
+      tr.classList.add("row", "row2", "row3", "p" + pId);
+
+      for (let i = 0; i < arr.length - 1; i++) {
+        const td = document.createElement("td");
+        if (i === 1) {
+          const img = document.createElement("img");
+          img.src = arr[i];
+          td.className = "imgTd";
+          td.appendChild(img);
+        } else {
+          td.appendChild(document.createTextNode(arr[i]));
+        }
+        tr.appendChild(td);
+      }
+      tbdy.appendChild(tr);
+    }
+
+    // Iterate through each player
+    topScorers.forEach((el, i) => {
+      if (i < 10) {
+        const pId = el.player.id;
+        console.log(pId);
+        getStats(pId);
+
+        const pPhoto = el.player.photo;
+        const pName = el.player.name;
+
+        addRow([pName, pPhoto, pId], pId);
+      }
+    });
+
+    // Append the table body element to the table element
+    tbl.appendChild(tbdy);
+
+    // Get the correct div element based on leagueId and append the table element to it
+    let divId;
+    if (leagueId === 39) divId = "divPremierLeague";
+    if (leagueId === 61) divId = "divLigue1";
+    if (leagueId === 253) divId = "divMls";
+
+    const div = document.getElementById(divId);
+    div.appendChild(tbl);
+
+    // turn off spinner since data is in
+    const spinner = div.querySelector(".spinner");
+    if (spinner) {
+      console.log(spinner);
+      spinner.style.display = "none";
+    } else {
+      console.error("Spinner element not found");
+    }
+  }
   
   async function createTables() {
-    const arrOfLeagues = [39, 61, 253];  
-    // const arrOfLeagues = [39];  
+    document
+      .querySelectorAll(".spinner")
+      .forEach((spinner) => (spinner.style.display = "block"));
+
+    const arrOfLeagues = [39, 61, 253];
+    // const arrOfLeagues = [39];
     for (const id of arrOfLeagues) {
       try {
         const topScorers = await getTopScorers(id);
@@ -29,79 +114,7 @@ const requestOptions = {
     }
   }
 
-
-
-createTables();
-
-// ---------------------------------------------------------------------------------------
-function createTable(topScorers, leagueId) {
-  // Create table element and set its class and border attribute
-  const tbl = document.createElement("table");
-  tbl.className = "leagueId" + leagueId;
-  tbl.setAttribute("border", "1");
-
-  // Create table body element
-  const tbdy = document.createElement("tbody");
-
-  // Create table header row
-  const headerRow = document.createElement("tr");
-  headerRow.className = "headingRow";
-
-  // Create header cells containing the column headings and add them to the header row
-  const headerCells = ["", "", "", "Goals", "On Target", "Assists"];
-  headerCells.forEach((headerCell) => {
-    const td = document.createElement("td");
-    td.textContent = headerCell;
-    headerRow.appendChild(td);
-  });
-  tbdy.appendChild(headerRow);
-
-  // Function to add a row to the table (Moved out of the forEach loop)
-  function addRow(arr, pId) {
-    const tr = document.createElement("tr");
-    tr.classList.add("row", "row2", "row3", "p" + pId);
-
-    for (let i = 0; i < arr.length - 1; i++) {
-      const td = document.createElement("td");
-      if (i === 1) {
-        const img = document.createElement("img");
-        img.src = arr[i];
-        td.className = "imgTd";
-        td.appendChild(img);
-      } else {
-        td.appendChild(document.createTextNode(arr[i]));
-      }
-      tr.appendChild(td);
-    }
-    tbdy.appendChild(tr);
-  }
-
-  // Iterate through each player
-  topScorers.forEach((el, i) => {
-    if (i < 10) {
-      const pId = el.player.id;
-      console.log(pId);
-      getStats(pId);
-
-      const pPhoto = el.player.photo;
-      const pName = el.player.name;
-
-      addRow([pName, pPhoto, pId], pId);
-    }
-  });
-
-  // Append the table body element to the table element
-  tbl.appendChild(tbdy);
-
-  // Get the correct div element based on leagueId and append the table element to it
-  let divId;
-  if (leagueId === 39) divId = 'divPremierLeague';
-  if (leagueId === 61) divId = 'divLigue1';
-  if (leagueId === 253) divId = 'divMls';
-
-  const div = document.getElementById(divId);
-  div.appendChild(tbl);
-}
+  createTables();
 
 // GET STATS FOR EACH PLAYER AND ADD TO ROW THAT IS ALREADY CREATED
 //  ----------------------------------------------------------------------------------------------------
